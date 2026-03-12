fa820f3102f425785b64569016f789559253a592
Mattia Mion <mattia.mion@gmail.com>
2026-03-12 11:16:22 +0100

    feat: aggiunta loader attesa chiamata axios

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -12,35 +12,51 @@ function SearchPage() {
     const searched = searchParams.get("query");
     //var di stato per salvare prodotti cercati
     const [searchedItems, setSearchedItems] = useState([]);
+    //var di stato per caricameto chiamata axiois
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        setIsLoading(true);
         //salvo endpoint dentro una costante
         const endpoint = `http://localhost:3000/api/products?search=${searched}` //uso backtic per inserrie van "searched" dentro la stringa
         //chiamata axios
         axios.get(endpoint)
             .then((res) => {
-                setSearchedItems(res.data)
+                setSearchedItems(res.data);
+                setIsLoading(false);
             })
             .catch((err) => console.log(err));
+        setIsLoading(false);
     }, [searched]);
+
+    //se is loading é true gestisci il caricamento
+    if (isLoading) {
+        return (
+            <div className="loader-container">
+                <h2>Caricamento in corso...</h2>
+            </div>
+        );
+    }
 
     return (
         <div>
             {searchedItems.length > 0 ? (
                 <div>
                     <h2>risultati per: {searched}</h2>
-                    {searchedItems.map(item => <div className="card-container" key={item.id}>
-                        <div className="img-container">
-                            <img className="card-image" src={item.image} alt={item.name} />
-                        </div>
-                        <div className="text-container">
-                            <Link className="card-link" to={`/product/${item.slug}`}>
-                                {item.name}
-                            </Link>
-                            <div>{item.weight} g</div>
-                            <div className="card-price">prezzo: {item.price} &euro;</div>
-                        </div>
-                    </div>)}
+                    <div className="home-container">
+                        {searchedItems.map(item => <div className="card-container" key={item.id}>
+                            <div className="img-container">
+                                <img className="card-image" src={item.image} alt={item.name} />
+                            </div>
+                            <div className="text-container">
+                                <Link className="card-link" to={`/product/${item.slug}`}>
+                                    {item.name}
+                                </Link>
+                                <div>{item.weight} g</div>
+                                <div className="card-price">prezzo: {item.price} &euro;</div>
+                            </div>
+                        </div>)}
+                    </div>
                 </div>) : (<p>Nessun prodotto trovato per "{searched}"</p>)}
 
         </div>
