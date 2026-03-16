95401742fef31ad9488a184d31863ca16599a86c
Luca Giampieri <luca.giampieri95@gmail.com>
2026-03-16 15:45:01 +0100

    aggiunt alink regione

--- src/pages/RegionPage.jsx
+++ src/pages/RegionPage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useGlobal } from "../context/GlobalContext";
 import ProductCard from "../components/ProductCard";
+import { Link } from "react-router-dom";
 
 function RegionPage() {
 
@@ -46,45 +47,49 @@ function RegionPage() {
                     .sort(() => Math.random() - 0.5)
                     .map(region => (
                         <div className="region-card-container">
+                            <Link to={`/region/${region.name}/products`} >
+                                <div className="region-text-container">
+                                    <button
+                                        type="button"
+                                        className="card-link"
+                                        onClick={() => handleRegionClick(region.name)}
+                                    >
+                                        {region.name}
+                                    </button>
+                                </div>
 
-                            <div className="region-text-container">
-                                <button
-                                    type="button"
-                                    className="card-link"
-                                    onClick={() => handleRegionClick(region.name)}
-                                >
-                                    {region.name}
-                                </button>
-                            </div>
-
-                            <div className="region-img-container">
-                                <img
-                                    className="card-image"
-                                    src={region.image}
-                                    alt={region.name}
-                                />
-                            </div>
+                                <div className="region-img-container">
+                                    <img
+                                        className="card-image"
+                                        src={region.image}
+                                        alt={region.name}
+                                    />
+                                </div>
+                            </Link>
                         </div>
+
                     ))}
             </div>
 
-            {selectedRegion !== "" && (
-                <>
-                    <h2 className="home-subtitle">
-                        Prodotti della regione: {selectedRegion}
-                    </h2>
+            {
+                selectedRegion !== "" && (
+                    <>
+                        <h2 className="home-subtitle">
+                            Prodotti della regione: {selectedRegion}
+                        </h2>
 
-                    <div className="home-container">
-                        {productsRegion.map(product => (
-                            <ProductCard
-                                key={product.id}
-                                product={product}
-                            />
-                        ))}
-                    </div>
-                </>
-            )}
-        </main>
+                        <div className="home-container">
+                            {productsRegion.map(product => (
+                                <ProductCard
+                                    key={product.id}
+                                    product={product}
+                                />
+                            ))}
+                        </div>
+                    </>
+                )
+            }
+        </main >
     );
 }
 
