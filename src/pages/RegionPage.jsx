027df488cd650d895731b7f2109da998f42242e2
Luca Striano <luca.striano90@gmail.com>
2026-03-16 14:44:58 +0100

    tolto logica regions e add at globalcontext

--- src/pages/RegionPage.jsx
+++ src/pages/RegionPage.jsx
@@ -1,48 +1,14 @@
-//import axios
 import axios from "axios";
-
-//import useState e useEffect
 import { useState, useEffect } from "react";
-
-//import useGlobal
 import { useGlobal } from "../context/GlobalContext";
-
-//import Link
-import { Link } from "react-router-dom";
-
 import ProductCard from "../components/ProductCard";
-
 
 function RegionPage() {
 
-    //importiamo gli elementi che ci servono tramite la useContext
-    const { setIsLoading } = useGlobal();
+    const { regions, fetchRegions, setIsLoading } = useGlobal();
 
-    //creazione varibile endpoint in un salvare l'API
-    const endpoint = "http://localhost:3000/api/regions";
-
-    //creazione varbile di stato come un array vuoto
-    const [regions, setRegions] = useState([]);
     const [selectedRegion, setSelectedRegion] = useState("");
     const [productsRegion, setProductsRegion] = useState([]);
-
-    //creiamo una funzione per gestire la chiamta axios alla rotta index
-    function fetchRegions() {
-
-        //facciamo in modo che all'avvio della chiamata la varibile di stato cambi in true e parta il Loader
-        setIsLoading(true)
-
-        axios.get(endpoint)
-            .then(res => { setRegions(res.data) })
-            .catch(err => {
-                console.log(err);
-            })
-            //facciamo in modo che a chiamta effettuata la varibile di stato torni false e scompaia il Loader
-            .finally(() => {
-                //metto questi secondi per verificare che funzioni
-                setTimeout(() => setIsLoading(false), 1000);
-            });
-    };
 
     function fetchRegionProducts(regionName) {
         setIsLoading(true);
@@ -73,19 +39,14 @@ function RegionPage() {
         }
     }, [selectedRegion]);
 
-
-
     return (
         <main>
             <div className="region-container">
                 {[...regions]
                     .sort(() => Math.random() - 0.5)
                     .map(region => (
-                        <div
-                            className="region-card-container"
-                            key={region.id}
-                            onClick={() => handleRegionClick(region.name)}
-                        >
+                        <div className="region-card-container">
+
                             <div className="region-text-container">
                                 <button
                                     type="button"
@@ -124,7 +85,7 @@ function RegionPage() {
                 </>
             )}
         </main>
-    )
+    );
 }
 
-export default RegionPage
+export default RegionPage;
