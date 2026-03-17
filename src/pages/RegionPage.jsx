e84c555bcd530b829a44f2acd80056f963da7514
Luca Striano <luca.striano90@gmail.com>
2026-03-17 15:49:53 +0100

    delete logica di region e presa dal global cotext

--- src/pages/RegionPage.jsx
+++ src/pages/RegionPage.jsx
@@ -1,44 +1,13 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useGlobal } from "../context/GlobalContext";
-import ProductCard from "../components/ProductCard";
 import { Link } from "react-router-dom";
 
 function RegionPage() {
-
-    const { regions, fetchRegions, setIsLoading } = useGlobal();
-
-    const [selectedRegion, setSelectedRegion] = useState("");
-    const [productsRegion, setProductsRegion] = useState([]);
-
-    function fetchRegionProducts(regionName) {
-        setIsLoading(true);
-
-        axios.get(`http://localhost:3000/api/regions/name/${encodeURIComponent(regionName)}/products`)
-            .then(res => {
-                setProductsRegion(res.data);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }
-
-    function handleRegionClick(regionName) {
-        setSelectedRegion(regionName);
-    }
+    const { regions, fetchRegions } = useGlobal();
 
     useEffect(() => {
         fetchRegions();
     }, []);
-
-    useEffect(() => {
-        if (selectedRegion !== "") {
-            fetchRegionProducts(selectedRegion);
-        }
-    }, [selectedRegion]);
 
     return (
         <main>
@@ -46,50 +15,30 @@ function RegionPage() {
                 {[...regions]
                     .sort(() => Math.random() - 0.5)
                     .map(region => (
-                        <div className="region-card-container">
-                            <Link to={`/region/${region.name}/products`} >
-                                <div className="region-text-container">
-                                    <button
-                                        type="button"
-                                        className="card-link"
-                                        onClick={() => handleRegionClick(region.name)}
-                                    >
-                                        {region.name}
-                                    </button>
-                                </div>
+                        <Link
+                            className="region-card-container"
+                            key={region.id}
+                            to={`/region/${encodeURIComponent(region.name)}/products`}
+                        >
+                            <div className="region-pin"></div>
 
-                                <div className="region-img-container">
-                                    <img
-                                        className="card-image"
-                                        src={region.image}
-                                        alt={region.name}
-                                    />
-                                </div>
-                            </Link>
-                        </div>
+                            <div className="region-img-container">
+                                <img
+                                    className="card-image"
+                                    src={region.image}
+                                    alt={region.name}
+                                />
+                            </div>
 
+                            <div className="region-text-container">
+                                <span className="region-name">
+                                    {region.name}
+                                </span>
+                            </div>
+                        </Link>
                     ))}
             </div>
-
-            {
-                selectedRegion !== "" && (
-                    <>
-                        <h2 className="home-subtitle">
-                            Prodotti della regione: {selectedRegion}
-                        </h2>
-
-                        <div className="home-container">
-                            {productsRegion.map(product => (
-                                <ProductCard
-                                    key={product.id}
-                                    product={product}
-                                />
-                            ))}
-                        </div>
-                    </>
-                )
-            }
-        </main >
+        </main>
     );
 }
 
